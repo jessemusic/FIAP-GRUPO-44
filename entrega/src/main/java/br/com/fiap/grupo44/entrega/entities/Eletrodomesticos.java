0192a3c20908dab99a32c8a41d50fa366714ea8c
HerickRaposo <herickraposo97@gmail.com>
2023-06-23 10:01:45 -0300

    Adicao de atributos

--- entrega/src/main/java/br/com/fiap/grupo44/entrega/entities/Eletrodomesticos.java
+++ entrega/src/main/java/br/com/fiap/grupo44/entrega/entities/Eletrodomesticos.java
@@ -1,8 +1,11 @@
 package br.com.fiap.grupo44.entrega.entities;
 
 public class Eletrodomesticos {
+    private Integer id;
     private String nome;
     private String voltagem;
+    private Double consumo;
+    private Double custo;
 
-    p
+
 }
