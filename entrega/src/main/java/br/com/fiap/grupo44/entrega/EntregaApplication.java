a231f7f1b480003942452da657d3fcb0d91173f0
jessemusic <jmc_cursos@hotmail.com>
2023-06-22 21:17:23 -0300

    INICIANDO PROJETO

---
+++ entrega/src/main/java/br/com/fiap/grupo44/entrega/EntregaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.grupo44.entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaApplication.class, args);
+	}
+
+}
---
+++ entrega/src/test/java/br/com/fiap/grupo44/entrega/EntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.grupo44.entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
