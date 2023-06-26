3e32706971463602e0fee28e8494197d89f26382
ernesto.sambango <ernesto.sambango@minfin.gov.ao>
2023-06-26 11:12:36 +0100

    Save implementado.

--- entrega/src/main/java/br/com/fiap/grupo44/entrega/dto/EnderecoDTO.java
+++ entrega/src/main/java/br/com/fiap/grupo44/entrega/dto/EnderecoDTO.java
@@ -1,18 +1,24 @@
 package br.com.fiap.grupo44.entrega.dto;
 
 import br.com.fiap.grupo44.entrega.entities.Endereco;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EnderecoDTO {
+	
 	private Long id;
 	private String rua;
 	private Integer numero; 
 	private String bairro;
 	private String cidade;
 	private String estado;
+	
+	public EnderecoDTO() {
+		
+	}
 	
 	public EnderecoDTO(Endereco endereco) {
 		this.id     = endereco.getId();
@@ -21,5 +27,18 @@ public class EnderecoDTO {
 		this.bairro = endereco.getBairro();
 		this.cidade = endereco.getCidade();
 		this.estado = endereco.getEstado();
+	}
+	
+	public Endereco getEndereco(EnderecoDTO enderecoDTO) {
+		System.err.println("MONTEI O OBJECTO ENDEREÇO.");
+		Endereco endereco=new Endereco();
+		endereco.setId(enderecoDTO.getId());
+		endereco.setBairro(enderecoDTO.getBairro());
+		endereco.setCidade(enderecoDTO.getCidade());
+		endereco.setEstado(enderecoDTO.getEstado());
+		endereco.setNumero(enderecoDTO.getNumero());
+		endereco.setRua(enderecoDTO.getRua());
+		
+		return endereco;
 	}
 }
--- entrega/src/main/java/br/com/fiap/grupo44/entrega/service/EnderecoService.java
+++ entrega/src/main/java/br/com/fiap/grupo44/entrega/service/EnderecoService.java
@@ -1,5 +1,8 @@
 package br.com.fiap.grupo44.entrega.service;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,13 +19,19 @@ public class EnderecoService {
 	@Autowired
 	private IEEnderecoRepository enderecoRepository;
 
+	public EnderecoDTO salvar(EnderecoDTO enderecoDTO) {
+		System.err.println("ENTREI NO SERVIÇO.");
+		Endereco endereco = this.enderecoRepository.save(enderecoDTO.getEndereco(enderecoDTO));
+		return new EnderecoDTO(endereco);
+	}
+	
 	public EnderecoDTO findById(Long id) {
 		Endereco endereco = enderecoRepository.findById(id).orElseThrow(() -> new ControllerNotFoundException("Endereço não encontrado"));
 		return new EnderecoDTO(endereco);
 	}
-	
-	public Page<EnderecoDTO> findAll(PageRequest pagina){
-        var enderecos = enderecoRepository.findAll(pagina);
-        return enderecos.map(endereco -> new EnderecoDTO(endereco));
+
+	public List<EnderecoDTO> findAll(){
+        var enderecos = enderecoRepository.findAll();
+        return enderecos.stream().map(endereco -> new EnderecoDTO(endereco)).toList();
     }
 }
