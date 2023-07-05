eeaf007634e407cf5740605e978cbc8c5bfee69c
ernesto.sambango <ernesto.sambango@minfin.gov.ao>
2023-07-05 02:40:53 +0100

    ACERTO DA PAGINAÇÃO E ATUALIZAÇÃO E PESQUISA

--- entrega/src/main/java/br/com/fiap/grupo44/entrega/service/EnderecoService.java
+++ entrega/src/main/java/br/com/fiap/grupo44/entrega/service/EnderecoService.java
@@ -1,8 +1,10 @@
 package br.com.fiap.grupo44.entrega.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import br.com.fiap.grupo44.entrega.dto.EnderecoDTO;
@@ -26,8 +28,37 @@ public class EnderecoService {
 		return new EnderecoDTO(endereco);
 	}
 
-	public List<EnderecoDTO> findAll(){
-        var enderecos = enderecoRepository.findAll();
-        return enderecos.stream().map(endereco -> new EnderecoDTO(endereco)).toList();
+	public List<EnderecoDTO> findAll(PageRequest pageRequest){
+        var enderecos = enderecoRepository.findAll(pageRequest);
+        if(!enderecos.isEmpty()) {
+        	return enderecos.getContent().stream().map(endereco -> new EnderecoDTO(endereco)).toList();        	
+        }
+        throw new ControllerNotFoundException("Nenhum Endereço para listar na pagina especificada.");
     }
+
+	public EnderecoDTO atualizar(EnderecoDTO enderecoDTO,Long id) {
+        Optional<Endereco> OPendereco = this.enderecoRepository.findById(id);
+        try {
+        	Endereco endereco = OPendereco.get();
+        	endereco.setBairro(enderecoDTO.getBairro());
+        	endereco.setCidade(enderecoDTO.getCidade());
+        	endereco.setEstado(enderecoDTO.getEstado());
+        	endereco.setNumero(enderecoDTO.getNumero());
+        	endereco.setRua(enderecoDTO.getRua());
+        	this.enderecoRepository.save(endereco);
+        	return new EnderecoDTO(endereco);
+        }catch (Exception e) {
+            throw new ControllerNotFoundException("Endereço não encontrado, id: " + id);
+		}
+	}
+
+	public String apagar(Long id) {
+		Optional<Endereco> OPendereco = this.enderecoRepository.findById(id);
+		if(OPendereco.isPresent()){
+			Endereco endereco = OPendereco.get();
+			this.enderecoRepository.delete(endereco);
+			return "Removido o endereço de ID: "+id;
+		}
+        throw new ControllerNotFoundException("Endereço não encontrado, id: " + id);
+	}
 }
