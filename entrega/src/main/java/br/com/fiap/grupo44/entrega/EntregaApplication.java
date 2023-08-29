9e755c3400045f42cc9c2e8d9a50db1057a22836
jessemusic <jmc_cursos@hotmail.com>
2023-08-29 00:28:00 -0300

    Ajuste no swagger

--- entrega/src/main/java/br/com/fiap/grupo44/entrega/EntregaApplication.java
+++ entrega/src/main/java/br/com/fiap/grupo44/entrega/EntregaApplication.java
@@ -1,14 +1,10 @@
 package br.com.fiap.grupo44.entrega;
 
-
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1",description = "PAI desenvolvida para testes PROJETO FIAP GRUPO-44"))
+//@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1",description = "API desenvolvida para testes PROJETO FIAP GRUPO-44"))
 public class EntregaApplication {
 
 	public static void main(String[] args) {
