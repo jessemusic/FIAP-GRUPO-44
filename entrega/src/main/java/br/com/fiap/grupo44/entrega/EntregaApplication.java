940ac571ac0ed0bbd7fdaa4ac3520c60ddb53a65
HerickRaposo <herickraposo97@gmail.com>
2023-08-29 22:10:19 -0300

    Commit parcial

--- entrega/src/main/java/br/com/fiap/grupo44/entrega/EntregaApplication.java
+++ entrega/src/main/java/br/com/fiap/grupo44/entrega/EntregaApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1",description = "PAI desenvolvida para testes PROJETO FIAP GRUPO-44"))
+//@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1",description = "PAI desenvolvida para testes PROJETO FIAP GRUPO-44"))
 public class EntregaApplication {
 
 	public static void main(String[] args) {
--- entrega/src/main/java/br/com/fiap/grupo44/entrega/dominio/eletrodomestico/controllers/EletrodomesticoController.java
+++ entrega/src/main/java/br/com/fiap/grupo44/entrega/dominio/eletrodomestico/controllers/EletrodomesticoController.java
@@ -5,6 +5,7 @@ import br.com.fiap.grupo44.entrega.dominio.eletrodomestico.services.Eletrodomest
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,6 +19,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/eletrodomesticos")
+@Tag(name = "open-apí")
 public class EletrodomesticoController {
 
 
